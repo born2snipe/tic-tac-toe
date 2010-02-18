5eeb1102695bcc4b7a6c04f0ab4d549f61e0b705
born2snipe <born2snipe@gmail.com>
2010-02-17 19:51:27 -0600

    updated to queue up messages to be displayed as notifications

--- src/main/java/b2s/tictactoe/Notification.java
+++ src/main/java/b2s/tictactoe/Notification.java
@@ -1,6 +1,8 @@
 package b2s.tictactoe;
 
 import java.awt.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 
 public class Notification implements Renderable, Tickable {
@@ -8,13 +10,14 @@ public class Notification implements Renderable, Tickable {
     private static final long TIME_FOR_VIEWING = 3000L;
     private static final int PADDING = 5;
     private final Dimension screenSize;
-    private String message;
+    private String currentMessage;
     private Dimension boxSize;
     private Point currentLocation;
     private boolean show = false;
     private long startViewingTime;
     private int fontHeight;
     private State state;
+    private Queue<String> messages = new LinkedList<String>();
 
     public Notification(Dimension screenSize) {
         this.screenSize = screenSize;
@@ -31,9 +34,19 @@ public class Notification implements Renderable, Tickable {
                 }
             } else if (state == State.MOVING_DOWN) {
                 currentLocation = new Point(0, currentLocation.y + 2);
-            } else {
+                if (isFullyHidden()) {
+                    show = false;
+                }
+            } else if (state == State.MOVING_UP) {
                 currentLocation = new Point(0, currentLocation.y - 2);
             }
+        } else if (isFullyHidden() && messages.size() > 0) {
+            boxSize = null;
+            state = State.MOVING_UP;
+            synchronized (messages) {
+                this.currentMessage = messages.remove();
+            }
+            show = true;
         }
     }
 
@@ -45,7 +58,7 @@ public class Notification implements Renderable, Tickable {
             // get the height of a line of text in this font and render context
             fontHeight = metrics.getHeight();
             // get the advance of my text in this font and render context
-            int adv = metrics.stringWidth(message);
+            int adv = metrics.stringWidth(currentMessage);
             // calculate the size of a box to hold the text with some padding.
             boxSize = new Dimension(adv + PADDING, fontHeight + PADDING);
 
@@ -57,15 +70,17 @@ public class Notification implements Renderable, Tickable {
         g.fillRect(currentLocation.x, currentLocation.y, boxSize.width, boxSize.height);
         g.drawRect(currentLocation.x - 3, currentLocation.y, boxSize.width + 3, boxSize.height);
         g.setColor(Color.red);
-        g.drawString(message, currentLocation.x + 2, currentLocation.y + fontHeight);
+        g.drawString(currentMessage, currentLocation.x + 2, currentLocation.y + fontHeight);
     }
 
     public void show(String message) {
-        show = true;
-        boxSize = null;
+        synchronized (messages) {
+            this.messages.add(message);
+        }
+    }
 
-        state = State.MOVING_UP;
-        this.message = message;
+    private boolean isFullyHidden() {
+        return currentLocation == null || currentLocation.y > screenSize.height;
     }
 
     private boolean isFullyVisible() {
@@ -73,6 +88,6 @@ public class Notification implements Renderable, Tickable {
     }
 
     private static enum State {
-        MOVING_UP, MOVING_DOWN, VIEWING
+        MOVING_UP, MOVING_DOWN, VIEWING, HIDDEN
     }
 }
